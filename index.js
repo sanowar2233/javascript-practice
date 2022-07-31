64cd01e86ecc023bd787a7d818a33bbbc37c7f6b
sanowar2233 <sanowarhossain1082@gmail.com>
2022-08-01 00:24:00 +0600

    javascript-practice

---
+++ index.js
@@ -0,0 +1,24 @@
+var money= 100;
+var danis= 41;
+var butter= 44;
+var toast= 96;
+
+if(danis < money){
+    console.log("danish khabo");
+}else if(butter < money){
+    console.log("butter khabo")
+
+}else if(toast < money){
+    console.log("toast khabo")
+}else{
+    console.log("ca khabo")
+}
+
+
+
+
+
+
+
+
+
