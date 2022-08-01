064150068b4c9e225d9983a2bcb9fa33ab88dec3
sanowar2233 <sanowarhossain1082@gmail.com>
2022-08-02 00:09:17 +0600

    java

--- index.js
+++ index.js
@@ -1,9 +1,383 @@
+Class-1#  30 july 2022
+
+1.
+// var onionPrice = 30;
+// var eggPrice=20;
+// var total= onionPrice + eggPrice;
+// console.log(total)
+
+2.
+// var number1= 34;
+// var number=5;
+// var total=(number1 / number);
+// console.log(total)
+
+3.
+//   var price1 = 45;
+//  price1 = price1 + 20;
+//  console.log(price1)
+
+4.
+// var age=5;
+// age++
+// console.log(age)
+
+5.
+// var price1=34;
+// var price2=45;
+// var total= price1 + price2;
+// console.log(total);
+
+6.
+// var firstName = "kim";
+// var lastName ="vai";
+// var total = firstName + " " + lastName;
+// console.log(total)
+
+7.
+// var number="31.5"
+//  var number1= parseFloat(number);
+//  console.log(number1)
+
+
+8.
+// var number = 34;
+// console.log(typeof number);
+
+9.
+// var num1=.1;
+// var num2=.3;
+// var total= num1 + num2;
+// total= total.toFixed(4)
+// console.log(total)
+
+10.
+// var mango=7;
+// var person=2;
+// console.log(mango % person)
+
+                                                                   Problem Solving
+
+1.
+// var harryMomGives =1000;
+// var apple=350;
+// var orange=350;
+// var total=apple + orange;
+// var total1= harryMomGives - (apple+orange);
+// console.log(total1)
+
+2.
+// var math=75;
+// var biology=25;
+// var chemistry=65;
+// var physics=80;
+// var bangla=35;
+// var total=math+biology+chemistry+physics+bangla;
+// console.log(total);
+// var average=total/5;
+// console.log(average.toFixed(2));
+
+
+3.
+// var firstLine= "I am going to be";
+// var lastLine="an awesome web developer"
+// var fullLine= firstLine + " " +lastLine
+// console.log(fullLine)
+
+
+4.
+// var num1=119;
+// var num2=5;
+// var total= num1%num2;
+// console.log(total)
+
+
+
+                                                       class 2#  31 July 2022
+
+1.
+// var friendAge=[23,45,76,78,3];
+***********************************
+// var man=["mahi", "apu" , "shabnur"];
+// console.log(man);
+
+2.
+// var number=[34,45,6,75,6];
+// console.log(number[0]);
+
+3.
+// var number=[45,67,78]
+// var element=number[2];
+// console.log(element)
+
+4.
+// var element=[34,5,67,6,54]
+// element[1]=54;
+// console.log(element)
+
+5.
+// var position= [34,56,7,67,8];
+// var positionindex=position.indexOf(34);
+// console.log(positionindex)
+
+6.
+// var number=[34,5,6,7,8];
+// number.push(63);
+// console.log(number)
+
+7.
+// var friends=["balam", "salam", "kalam"];
+// friends.push("jalam")
+// console.log(friends);
+
+8.
+// var number=[3,45,65,6,7];
+// number.pop();
+// number.pop()
+// console.log(number)
+
+***************
+var number=[34,54,6,78,6];
+number.unshift(100);
+console.log(number)
+
+***********
+var number=[34,43,45,43];
+number.shift();
+console.log(number)
+
+9.
+// console.log(5 < 6)
+// console.log(5 > 6)
+// console.log(5==6)
+// console.log(6==6);
+// console.log(5!=6);
+// console.log(5<=6);
+
+
+10.
+// var num1=34;
+// var num2=32;
+// var num3=12;
+// console.log(num1>num2 && num2<num3 && num1>num3)
+// console.log(num1>num2 || num2<num3 || num1>num3)
+
+11.
+// var  iphone=5000;
+// var budget=74333;
+// if(iphone<budget){
+//     console.log("kine felbo")
+// }
+
+12.
+
+// var  iphone=5000;
+// var budget=84333;
+// if(iphone<budget){
+//     console.log("kine felbo")
+// }else{
+//     console.log("hobe na")
+// }
+
+
+13.
+// var num1=34;
+// var num2=32;
+// var num3=12;
+// console.log(num1>num2 && num2<num3 && num1>num3)
+// console.log(num1>num2 || num2<num3 || num1>num3)
+
+14.
+// var  iphone=5000;
+// var budget=74333;
+// if(iphone<budget){
+//     console.log("kine felbo")
+// }
+
+15.
+// var  iphone=5000;
+// var budget=84333;
+// if(iphone<budget){
+//     console.log("kine felbo")
+// }else{
+//     console.log("hobe na")
+// }
+
+
+16.
+// var  graduated=true;
+// var salary=35000;
+// if(graduated==true || salary>50000){
+//     console.log("yes")
+
+// }
+// else{
+//     console.log("no")
+// }
+
+17
+// var  graduated=true;
+// var salary=35000;
+// if(graduated==true && salary>50000){
+//     console.log("yes")
+
+// }
+// else{
+//     console.log("no")
+// }
+
+
+18.
+// var  graduated=true;
+// var salary=34000;
+// var car =1;
+// if( (graduated==true && salary==20000)|| car<=2){
+//     console.log("hobe")
+// }
+
+19.
+// var money= 100;
+// var danis= 41;
+// var butter= 44;
+// var toast= 96;
+
+// if(danis < money){
+//     console.log("danish khabo");
+// }else if(butter < money){
+//     console.log("butter khabo")
+
+// }else if(toast < money){
+//     console.log("toast khabo")
+// }else{
+//     console.log("ca khabo")
+// }
+
+
+
+  
+1.                                                   problem solving*******************
+//var fruit=["apple", "orange", "banana"];
+// fruit.splice(2,1,"mango");
+// console.log(fruit)
+
+2.
+var firstnumber=13;
+// var secondnumber=79;
+// var thirdnumber=45;
+
+// if(firstnumber>secondnumber){
+//     console.log("first number is large")
+// }else if(thirdnumber>secondnumber){
+//     console.log("third number is large")
+// }else{
+//     console.log("second number is large")
+// }
+
+
+
+3.
+var num1=9;
+// var num2=8;
+// var num3=9;
+
+// if(num1==num2){
+//     console.log("num1 and num2 is equal")
+// }else if(num2==num3){
+//     console.log("num2 and num3 is equal")
+// }else{
+//     console.log("num1 and num3 is equal")
+// }
+
+
+                                                   3rd class 1 augest 2022
+
+1.
+// var roastgiven=0;
+// while (roastgiven < 7){
+//     console.log(roastgiven)
+//     console.log("please")
+//     roastgiven++;
+// }
+
+2.
+// var number=1;
+// while(number<10){
+//     console.log(number)
+//     number++;
+// }
+
+3.
+// var number = 1;
+// while(number<=10){
+//     console.log(number);
+//     number=number +2;
+// }
+
+4.
+// for(var  number=0; number < 10 ; number++){
+//     console.log('please')
+//     console.log(number)
+// }
+
+// for (var i=0; i<10; i++){
+//     console.log(i)
+// }
+
+
+5.
+// var numbers=[45,65,67,87,90,67];
+// for(var i=0;i<7; i++){
+//     var number=numbers[i]
+//     console.log(number)
+// }
+
+6.
+// var numbers=[45,65,78,98,93, 44, 23]
+// for(var i=0; i<numbers.length; i++){
+//     Number=numbers[i]
+//     console.log(Number)
+// }
+
+
+7.
+// for(var i =1 ; i<=20; i++){
+//     console.log(i);
+//     if(i > 10){
+//         break;
+//     }
+// }
+
+8.
+// var items=["shovon", "foysal", "robin","abul"];
+// for(i=0; i<items.length; i++){
+//     item=items[i];
+//     if(item=="foysal"){
+//         break;
+//     }
+//     console.log(item)
+// }
+
+
+9.
+// var numbers=[34,54,56,43,33,3]
+// for(var i=0; i< numbers.length; i++){
+//     var number=numbers[i];
+//     if(number >30){
+//         continue;
+//     }
+//     console.log(number)
+// }
+
+10.
 // var number=10;
 // while(number >0){
 //     console.log(number);
 //     number--;
 // }
 
-for(i=10; i>0; i--){
-    console.log(i)
-}
+
+11.
+
+// for(i=10; i>0; i--){
+//     console.log(i)
+// }
